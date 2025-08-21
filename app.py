f5821cb8d11ffd04473a506171f3c28ead6f0f23
Manuel Guillén <76620339+Manumanito23@users.noreply.github.com>
2025-08-21 15:39:05 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def render_phase3d_png(
     ax2.grid(True)
 
     fig.suptitle(f"{name} — PAIG 3D phase projections", fontsize=14)
-    #fig.tight_layout(rect=[0, 0, 0, 0])
+    fig.tight_layout(rect=[1, 1, 1, 1])
 
     buf = io.BytesIO()
     fig.savefig(buf, format="png", dpi=dpi, bbox_inches="tight")
