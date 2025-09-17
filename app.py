dfe2348afab9c2bbe2d5df820b5d712c96eec8e0
Manuel Guillén <76620339+Manumanito23@users.noreply.github.com>
2025-09-17 13:03:13 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ with col_right:
                 sol_plot = sol
             png = render_fit_png(
                 program_name,
-                t_plot_years if not label_shift_on else (t_plot_years - 1),
+                t_plot_years if not label_shift_on else (t_plot_years + 1),
                 df_sorted,
                 sol_plot,
                 dpi=150,
